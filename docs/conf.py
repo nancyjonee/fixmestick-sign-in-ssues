967fa24b16274bfc65e8b32399558e535c3855fa
nancyjonee <nancyjone05@gmail.com>
2025-06-23 15:21:57 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = 'fixmestick-sign-in-ssues'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
